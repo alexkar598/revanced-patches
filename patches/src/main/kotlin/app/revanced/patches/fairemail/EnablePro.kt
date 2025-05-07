cfcbc33a5894f449406704014468d07799bcb9e7
alexkar598 <25136265+alexkar598@users.noreply.github.com>
2025-05-07 00:18:22 -0400

    Adds enablepro patch for fairemail

---
+++ patches/src/main/kotlin/app/revanced/patches/fairemail/EnablePro.kt
@@ -0,0 +1,22 @@
+package app.revanced.patches.fairemail
+
+import app.revanced.patcher.extensions.InstructionExtensions.addInstructions
+import app.revanced.patcher.patch.bytecodePatch
+
+@Suppress("unused")
+val enableProPatch = bytecodePatch(
+    name = "Enable Pro features",
+    description = "Enable Pro features",
+) {
+    compatibleWith("eu.faircode.email")
+
+    execute {
+        isProFingerprint.method.addInstructions(
+            0,
+            """
+                const/4 v0, 0x1
+                return v0
+            """,
+        )
+    }
+}
